1354ba7f8e2d3cf0a52bfc966ef55c041f253f43
Moritz Breuss <BRE230090@spengergasse.at>
2024-11-10 12:31:47 +0100

    Hausaufgabe hochladen 2024-10-11

--- 2024-11-05 Webseite Blumengeschäft Shop mit JavaScript/script.js
+++ 2024-11-05 Webseite Blumengeschäft Shop mit JavaScript/script.js
@@ -1,57 +1,44 @@
-selection()
+window.onload = function(){
+    selection();
+}
 let sum = 0;
 
-function selection(){
-    let productDropdown = document.getElementById("product");
-
-
-    let productPrice = productDropdown.value;
+function selection() {
+    const productDropdown = document.getElementById("product");
+    const productPrice = productDropdown.value;
     document.getElementById("output").innerHTML = productPrice + "€";
-    
 }
 
-function add(){
+function add() {
+    const productDropdown = document.getElementById("product");
+    const productName = productDropdown.options[productDropdown.selectedIndex].text;
+    const productPrice = Number(productDropdown.value);
 
-    let productDropdown = document.getElementById("product");
-
-    let productName = productDropdown.options[productDropdown.selectedIndex].text;
-    let productPrice = Number(productDropdown.value);
-
-    let tabelle = document.getElementById("table");
-
-    let reihe = tabelle.insertRow();
-
-    let name = reihe.insertCell(0);
-    let preis = reihe.insertCell(1);
+    const tabelle = document.getElementById("table");
+    const reihe = tabelle.insertRow();
+    const name = reihe.insertCell(0);
+    const preis = reihe.insertCell(1);
 
     name.innerHTML = productName;
-    preis.innerHTML = productPrice + "€";
+    preis.innerHTML = productPrice.toFixed(2) + "€";
 
     sum += productPrice; 
-
-    document.getElementById("betrag").innerHTML =sum.toFixed(2) + "€";
+    document.getElementById("betrag").innerHTML = sum.toFixed(2) + "€";
 }
-
 
 function alertFunction(event) {
     event.preventDefault();
-    let email = document.getElementById("email").value;
-    let username = document.getElementById("username").value;
-    let total = 0;
-    
-    total = sum;
+    const email = document.getElementById("email").value;
+    const username = document.getElementById("username").value;
 
-
-    if(username == "") {
+    if (username === "") {
         alert("Bitte geben Sie ihren Namen ein!");
-    }
-    else if(email == "") {
+    } else if (email === "") {
         alert("Bitte geben Sie ihre Emailadresse ein!");
-    }
-    else if(total === 0){
+    } else if (sum === 0) {
         alert("Bitte fügen Sie etwas in ihren Warenkorb hinzu!");
-    }
-    else {
-        alert("Ihre Bestellung " + username + " im Wert von " + total + "€ " + "wurde erfolgreich aufgegeben. Die Rechnung wurde an die E-mailadresse " + email + " gesendet.");
+    } else {
+        alert("Ihre Bestellung " + username + " im Wert von " + sum.toFixed(2) + "€ " +
+            "wurde erfolgreich aufgegeben. Die Rechnung wurde an die E-Mail-Adresse " + email + " gesendet.");
     }
 }
