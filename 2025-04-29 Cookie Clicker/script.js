f69196de4bb742dc0c4c9b98fe835ec65ce9acda
Moritz Breuss <BRE230090@spengergasse.at>
2025-05-20 17:20:47 +0200

    Upload API

--- 2025-04-29 Cookie Clicker/script.js
+++ 2025-04-29 Cookie Clicker/script.js
@@ -2,16 +2,22 @@
 const svgSolana = document.getElementById('my-svg');
 const svgUpgrade1 = document.getElementById('Upgrade1');
 const svgUpgrade2 = document.getElementById('Upgrade2');
+const svgUpgrade3 = document.getElementById('Upgrade3');
+const svgUpgrade4 = document.getElementById('Upgrade4');
 const svgAutoClicker1 = document.getElementById('AutoClicker1');
 const svgAutoClicker2 = document.getElementById('AutoClicker2');
+const svgAutoClicker3 = document.getElementById('AutoClicker3');
 const resetButton = document.getElementById('resetButton');
 const ascendButton = document.getElementById('ascendButton');
 
 // Prices
 let upgrade1Price = 50;  
-let upgrade2Price = 15000;  
+let upgrade2Price = 15000;
+let upgrade3Price = 1000000;
+let upgrade4Price = 10000000;
 let autoClicker1Price = 25000;  
 let autoClicker2Price = 100000;  
+let autoClicker3Price = 1000000;
 let ascendPrice = 50000000;
 
 
@@ -39,16 +45,20 @@ let upgrade2Purchased = false;
 
 // Multiplier
 let autoClicker1Count = 0;
+let autoClicker2Count = 0;
 let autoClicker2Multiplier = 1;
 let Upgrade1Multiplier = 1.1;
 let upgrade2Multiplier = 1.0;
+let upgrade3Multiplier = 1.0;
 let ascendMultiplier = 1.0;
 
 // Price Multipliers
 let upgrade1PriceMultiplier = 1.5;
 let upgrade2PriceMultiplier = 1.5;
+let upgrade3PriceMultiplier = 1.5;
 let autoClicker1PriceMultiplier = 1.5;
 let autoClicker2PriceMultiplier = 1.5;
+let autoClicker3PriceMultiplier = 1.5;
 
 let clicksPerSecond = 0;
 let clicksThisSecond = 0;
@@ -58,13 +68,17 @@ function updateUI() {
     document.getElementById('amountPerClick').textContent = `Amount per click: ${amountPerClick.toFixed(2)}`;
     document.getElementById('amountPerSecond').textContent = `Amount per second: ${amountPerSecond.toFixed(2)}`;
     document.getElementById('Upgrade1Price').textContent = `Cursor Price: ${upgrade1Price}`;
-    document.getElementById('Upgrade2Price').textContent = `AutoClicker Price: ${upgrade2Price}`;
+    document.getElementById('Upgrade2Price').textContent = `Grandma Upgrade Price: ${upgrade2Price}`;
     document.getElementById('AutoClicker1Price').textContent = `Grandma Price: ${autoClicker1Price}`;
     document.getElementById('AutoClicker2Price').textContent = `Factory Price: ${autoClicker2Price}`;
 }
 
 function updateProductionAutoClicker1() {
     amountPerSecondPassivAutoClicker1 = autoClicker1Count * upgrade2Multiplier * ascendMultiplier;
+}
+
+function updateProductionAutoClicker2() {
+    amountPerSecondPassivAutoClicker2 = autoClicker2Count * upgrade3Multiplier * ascendMultiplier;
 }
 
 svgSolana.addEventListener('click', () => {
@@ -109,6 +123,23 @@ svgUpgrade2.addEventListener('click', () => {
     }
 });
 
+svgUpgrade3.addEventListener('click', () => {
+    if(amount >= upgrade3Price) {
+        if(autoClicker2Count > 0) {
+            amount -= upgrade3Price;
+            autoClicker2Count += 10;
+            upgrade3Multiplier += 0.1;
+            upgrade3Price = Math.floor(upgrade3Price * upgrade3PriceMultiplier);
+            updateProductionAutoClicker2();
+            updateUI();
+        } else {
+            alert("Du musst zuerst AutoClicker 2 kaufen!");
+        }
+    } else {
+        alert("Nicht genug Solana!");
+    }
+});
+
 svgAutoClicker1.addEventListener('click', () => {
     if (amount >= autoClicker1Price) {
         amount -= autoClicker1Price;
@@ -124,6 +155,7 @@ svgAutoClicker1.addEventListener('click', () => {
 svgAutoClicker2.addEventListener('click', () => {
     if (amount >= autoClicker2Price) {
         amount -= autoClicker2Price;
+        autoClicker2Count += 1;
         amountPerSecondPassivAutoClicker2 += 10 * ascendMultiplier;
         autoClicker2Price = Math.floor(autoClicker2Price * autoClicker2PriceMultiplier);
         updateUI();
